f6695036496992326fe4df69f84ff631d7b80454
ARuslanDenisov <angess47@gmail.com>
2024-09-11 20:53:23 +0700

    03

---
+++ CustomShop/Models/OptionModel.swift
@@ -0,0 +1,15 @@
+//
+//  OptionModel.swift
+//  CustomShop
+//
+//  Created by Руслан on 11.09.2024.
+//
+
+import Foundation
+
+
+struct OptionModel: Identifiable, Codable {
+    var id: String = ""
+    var name: String = ""
+    var amount: Int = 0
+}
